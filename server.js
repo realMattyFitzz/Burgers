ef0115dc254e7ff18b48260eda14f1fe828325ac
Matthew Fitzpatrick <realmattyfitzz@Matthews-MacBook-Pro.local>
2020-10-19 10:37:39 -0700

    mit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const exphbs = require("express-handlebars")
 const router = require("./controllers/burgers_controller.js")
 
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 6060;
 
 const app = express();
 
